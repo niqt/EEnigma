5bd49b1df81880e3a059b2934c4183e51ed62716
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-10-06 19:00:27 +0200

    Initial Commit

---
+++ eenigma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eenigma
+//
+//  Created by Nicola De Filippo on 06/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eenigma/eenigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  eenigmaApp.swift
+//  eenigma
+//
+//  Created by Nicola De Filippo on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct eenigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
